09472f4893199959557761710f4dcec24d54a170
enteresanlikk <zeki1198@gmail.com>
2024-12-16 16:33:35 +0300

    Init buy me a coffe button.

---
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -0,0 +1,7 @@
+import { BuyMeACoffeeButton } from "@/components/common/buy-me-a-coffee-button";
+
+export default function HomePage() {
+  return <div>
+    <BuyMeACoffeeButton />
+  </div>;
+}
---
+++ src/components/common/buy-me-a-coffee-button.tsx
@@ -0,0 +1,7 @@
+export const BuyMeACoffeeButton = () => {
+    return (
+        <a href="https://www.buymeacoffee.com/bilaldemir" target="_blank">
+            <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
+        </a>
+    )
+}
